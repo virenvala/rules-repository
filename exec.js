bdda9642058767cdfec9eb2881d961e4c4dd3a1d
Viren <viren.vala@in.ibm.com>
2023-11-30 17:04:54 +0530

    exec tostring

--- exec.js
+++ exec.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 
 try {
-    let sts = execSync(`ps -ef | grep jenkins`);
+    let sts = execSync(`ps -ef | grep jenkins`).toString();
     console.log(sts);
 } catch(err) {
     console.log(err.status);
