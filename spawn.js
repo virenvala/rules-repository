af6104700ccfed3a9ecbec2434eb761a5392b6ce
Viren <viren.vala@in.ibm.com>
2023-11-30 16:31:59 +0530

    ps grep test

--- spawn.js
+++ spawn.js
@@ -1,6 +1,6 @@
 const { spawnSync } = require('node:child_process');
 //const ls = spawnSync('ls', ['-lh', '/usr']);
-const ls = spawnSync('ps -ef | grep jenkins');
+const ls = spawnSync('ps', ['-ef', '|', 'grep', 'jenkins']);
 
 /*ls.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`);
