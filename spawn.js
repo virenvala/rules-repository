97fbdd74fe49bc90efdfcbf97ef164b06a1d568d
Viren <viren.vala@in.ibm.com>
2023-11-30 16:37:11 +0530

    spawn ps

--- spawn.js
+++ spawn.js
@@ -1,8 +1,8 @@
-const { spawnSync } = require('node:child_process');
+const { spawn } = require('node:child_process');
 //const ls = spawnSync('ls', ['-lh', '/usr']);
-const ls = spawnSync('ps', ['-ef', '|', 'grep', 'jenkins']);
+const ls = spawn('ps', ['-ef', '|', 'grep', 'jenkins']);
 
-/*ls.stdout.on('data', (data) => {
+ls.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`);
 });
 
@@ -12,8 +12,8 @@ ls.stderr.on('data', (data) => {
 
 ls.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
-});*/
-console.log(ls.stdout.toString());
+});
+/*console.log(ls.stdout.toString());
 console.log(ls.status.toString());
 console.log(ls.stderr.toString());
-console.log(ls.output.toString());
+console.log(ls.output.toString());*/
