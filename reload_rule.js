4d70d6308e0b064fd70059b39e9186dbe1f68808
Viren <viren.vala@in.ibm.com>
2023-12-12 14:40:23 +0530

    formatted file

--- reload_rule.js
+++ reload_rule.js
@@ -7,7 +7,13 @@ var args = process.argv.slice(2);
 
 // primary
 try {
-    let sts = execSync(`ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${process.env.TEST_PRIVATE_KEY} netcool@10.23.97.129 | mpstatus`).toString();
+    let sts = execSync(`ssh 
+                -o StrictHostKeyChecking=no 
+                -o UserKnownHostsFile=/dev/null 
+                -i ${process.env.TEST_PRIVATE_KEY} 
+                netcool@10.23.97.129 
+                | 
+                mpstatus`).toString();
     console.log(sts);
 } catch(err) {
     console.log(err.status);
