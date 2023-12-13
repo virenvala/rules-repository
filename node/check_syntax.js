d22b15676ae8808e6074db7af70a8fa958c780e6
Viren <viren.vala@in.ibm.com>
2023-12-13 14:10:45 +0530

    syntax check update

--- node/check_syntax.js
+++ node/check_syntax.js
@@ -11,15 +11,19 @@ allFileContents.split(/\r?\n/).forEach(line => {
 
     // for the instance, invoke check syntax script on remote server
     try {
-        //let sts = execSync(`ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${process.env.PRIVATE_KEY} | /path/to/syntax/check/script -server ${server} ${line}`).toString();
-        exec(`ssh -o StrictHostKeyChecking=no -i /home/virenvala/.ssh/id_rsa netcool@10.23.97.129 /usr/tivoli/netcool/omnibus/probes/nco_p_syntax -server C_D_COL_D4_P1 -rulesfile /usr/tivoli/netcool/omnibus/etc/custom/MOBILE/rules/Ericsson_Generic_3GPP/esn_ossrc_3gpp_corba_3g_riy.rules`, function(err, stdout, stderr) {
+        //let sts = execSync(`ssh -o StrictHostKeyChecking=no -i /home/virenvala/.ssh/id_rsa netcool@10.23.97.129 "source /etc/profile; /usr/tivoli/netcool/omnibus/probes/nco_p_syntax -server C_D_COL_D4_P1 -rulesfile /usr/tivoli/netcool/omnibus/etc/custom/MOBILE/rules/Ericsson_Generic_3GPP/esn_ossrc_3gpp_corba_3g_riy.rules"`);
+        //exec(`ssh -o StrictHostKeyChecking=no -i /home/virenvala/.ssh/id_rsa netcool@10.23.97.129 /home/netcool/test_syntax.sh`, function(err, stdout, stderr) {
+        exec(`ssh -o StrictHostKeyChecking=no -i /home/virenvala/.ssh/id_rsa netcool@10.23.97.129 "source /etc/profile; /usr/tivoli/netcool/omnibus/probes/nco_p_syntax -server C_D_COL_D4_P1 -rulesfile /usr/tivoli/netcool/omnibus/etc/custom/MOBILE/rules/Ericsson_Generic_3GPP/esn_ossrc_3gpp_corba_3g_riy.rules"`, { maxBuffer: 1000 * 1000 * 10 }, function (err, stdout, stderr) {
             if (err) {
                 console.log("error---");
                 console.log(err);
             }
             console.log(stdout);
+            if (stdout.indexOf("Rules file syntax OK") > 0) {
+                console.log("syntax ok");
+            }
         });
-        
+
     } catch (err) {
         console.log("error-----");
         console.log(err);
